18ac4be98f9a4a79f8212c7aa0a64b33fb7061ec
Gordon Weitz <sonyshot3@hotmail.com>
2017-01-20 16:26:51 -0800

    Alien base code

---
+++ Asteroids/Alien.cpp
@@ -0,0 +1,39 @@
+#include "Alien.h"
+
+Alien::Alien(float x, float y, float speed): m_position(x, y), m_speed(speed) {
+	m_sprite.setPosition(m_position);
+	m_sprite.setSize(sf::Vector2f(60.f, 30.f));
+};
+
+Alien::~Alien() {
+	//delete any blaster/laser objects
+}
+
+void Alien::move() {
+	if (m_numMoves % 5 == 0) {
+		m_position.y -= 10;
+		m_sprite.move(0, -10);
+		m_speed *= -1;
+		m_numMoves++;
+	}
+	else {
+		m_position.x += m_speed;
+		m_sprite.move(m_speed, 0);
+		m_numMoves++;
+	}
+}
+
+void Alien::shoot() {
+	if (rand() % 10 < m_shootFreq) {
+		//create a laser/blaster object
+	}
+}
+
+void Alien::update() {
+	move();
+	shoot();
+}
+
+void Alien::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+	target.draw(m_sprite, states);
+}
--- Asteroids/Alien.h
+++ Asteroids/Alien.h
@@ -1,11 +1,25 @@
 #include <SFML\Graphics.hpp>
+#include <stdlib.h>
+#include <time.h>
 
 class Alien : public sf::Drawable {
 	sf::Vector2f m_position;
 	float m_speed;
+	sf::RectangleShape m_sprite;
+	int m_numMoves = 0;
+	int m_shootFreq = 5;
 
+	void move();
+
+	void shoot();
 
 public:
+	Alien(float x, float y, float speed);
+
+	~Alien();
+
+	void update();
+
 	//this draw overrides class sf::Drawable's draw function and allows "window.draw(Asteroid obj)" to draw to screen
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 };
