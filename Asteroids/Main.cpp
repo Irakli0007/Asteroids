a0bb5c977220486d73a31c4275652edaa0773df7
sonyshot <sonyshot7@gmail.com>
2017-01-24 17:47:21 -0800

    gitignore hates me

--- Asteroids/Main.cpp
+++ Asteroids/Main.cpp
@@ -1,23 +1,28 @@
 #include "Alien.h"
 
 int main() {
+
+	float fps = 120;
+	sf::Time sleepTime(sf::seconds(1.f/fps));
+	sf::Clock clock;
+
 	Player player(400, 0);
-	Alien alien1(500, 400, 4, &player);
-	Alien alien2(450, 400, 4, &player);
-	Alien alien3(550, 400, 4, &player);
+	Alien alien1(500, 420, 3, &player);
+	Alien alien2(450, 360, 4, &player);
+	Alien alien3(550, 400, 5, &player);
 
 	sf::RenderWindow window(sf::VideoMode(800, 800), "Space Invaders");
 	int counter = 0;
 
 	while (window.isOpen()) {
+		clock.restart();
+
 		sf::Event event;
-		if (counter == 15) {
-			player.move();
-			alien1.update();
-			alien2.update();
-			alien3.update();
-			counter = 0;
-		}
+		player.move();
+		alien1.update();
+		alien2.update();
+		alien3.update();
+		
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed) {
 				window.close();
@@ -29,6 +34,10 @@ int main() {
 		window.draw(alien3);
 		window.draw(player);
 		window.display();
+
+		if (clock.getElapsedTime() < sleepTime)
+			sf::sleep(sleepTime - clock.getElapsedTime());
+
 		counter++;
 	}
 
